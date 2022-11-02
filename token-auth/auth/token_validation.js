587dfa0a82db7a7b864f698da7a167267960cc96
Dainy_Jose <103561567+da-ny-jose@users.noreply.github.com>
2022-11-02 12:04:15 +0530

    Create token_validation.js

---
+++ token-auth/auth/token_validation.js
@@ -0,0 +1,29 @@
+const { verify } = require("jsonwebtoken");
+
+module.exports = {
+    checkToken: (req, res, next) => {
+        let token = req.get("authorization");
+        if (token){
+            //Remove Bearer from String
+            token = token.slice(7);
+            verify(token,process.env.JWT_KEY,(err, decoded) =>{
+                if (err){
+                    res.json({
+                        success:0,
+                        message:"Invalid token"
+                    });
+                }
+                else{
+                    req.decoded=decoded;
+                    next();
+                }
+            });
+        }
+        else{
+            res.json({
+                success:0,
+                message:"Access denied! unautorized user"
+            });
+        }
+    }
+};
