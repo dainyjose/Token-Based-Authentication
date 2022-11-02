e1b18be87f3453126d3b5501e4aa1c48926122e7
Dainy_Jose <103561567+da-ny-jose@users.noreply.github.com>
2022-11-02 12:03:22 +0530

    Create user.service.js

---
+++ token-auth/api/users/user.service.js
@@ -0,0 +1,32 @@
+const pool = require("../../config/database");
+
+module.exports = {
+
+  getUserByUserId: (id, callBack) => {
+    pool.query(
+        `select id,firstName,lastName,gender,email,number from registration`,
+
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+ 
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `select * from registration where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+
+};
